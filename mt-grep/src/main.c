2ec15bfabc51e6f7b67d0c50fb274b145ae5278d
yeah6161 <barirampage21@gmail.com>
2025-11-22 07:32:57 +0700

    Update main.c

--- mt-grep/src/main.c
+++ mt-grep/src/main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fsearch.h"
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("Usage: %s <dir> <pattern>\n", argv[0]);
+        return 1;
+    }
+    search_in_dir(argv[1], argv[2]);
+    return 0;
+}
 
