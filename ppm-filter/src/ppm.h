f5b75aed6cd9ac7c1df8f6429e189e96f20c8f89
yeah6161 <barirampage21@gmail.com>
2025-11-22 07:40:19 +0700

    Update ppm.h

--- ppm-filter/src/ppm.h
+++ ppm-filter/src/ppm.h
@@ -1,1 +1,9 @@
+#ifndef PPM_H
+#define PPM_H
+typedef struct { int w,h; unsigned char *data; } Image;
+Image *read_ppm(const char *path);
+int write_ppm(const char *path, Image *img);
+Image *grayscale(Image *img);
+void free_image(Image *img);
+#endif
 
