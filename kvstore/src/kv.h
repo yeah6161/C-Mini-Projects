f07c2d9a3369512d7551c55085d678d5bf6e5061
yeah6161 <barirampage21@gmail.com>
2025-11-22 07:35:02 +0700

    Update kv.h

--- kvstore/src/kv.h
+++ kvstore/src/kv.h
@@ -1,1 +1,10 @@
+#ifndef KV_H
+#define KV_H
+int kv_set(const char *k, const char *v);
+const char *kv_get(const char *k);
+int kv_delete(const char *k);
+int kv_save(const char *path);
+int kv_load(const char *path);
+void kv_free();
+#endif
 
